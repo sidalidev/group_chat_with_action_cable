bc27c13d379ee69ac9e05a24c634a6f6563ff3cb
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-08-09 07:16:04 +0200

    g ChannelUser Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_051353) do
+ActiveRecord::Schema.define(version: 2021_08_09_051533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channel_users", force: :cascade do |t|
+    t.bigint "channel_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["channel_id"], name: "index_channel_users_on_channel_id"
+    t.index ["user_id"], name: "index_channel_users_on_user_id"
+  end
 
   create_table "channels", force: :cascade do |t|
     t.string "name"
@@ -33,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_08_09_051353) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "channel_users", "channels"
+  add_foreign_key "channel_users", "users"
 end
