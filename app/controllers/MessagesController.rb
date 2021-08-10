c2415a004c48710a51e8904dfe3c06983019f87f
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-08-10 07:42:57 +0200

    messages: send messages no realtime

---
+++ app/controllers/MessagesController.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_channel
+
+  def create
+    @message = @channel.messages.create message_params
+  end
+
+  private 
+    def set_channel
+      @channel = current_user.channels.find params[:channel_id] # inforce the user is a member of the channel
+    end
+
+    def message_params
+      params.require(:message).permit(:body).merge user: current_user
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :channels do
     resource :channel_user # Singular, if we want to create/delete a user channel, we will operate on the current user without passin an extra ID for the user itself.
+    resources :messages
   end
 
   devise_for :users
