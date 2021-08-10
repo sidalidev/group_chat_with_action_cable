048f6b8bdf82e65d72e23bac289337afae106c2d
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-08-10 07:05:42 +0200

    channel: rework show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :channels
+  resources :channels do
+    resource :channel_user # Singular, if we want to create/delete a user channel, we will operate on the current user without passin an extra ID for the user itself.
+  end
+
   devise_for :users
 
   root to: 'channels#index'
