d6a7b625440fea1148f11c1d0434593747aa70c4
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-08-18 11:37:30 +0200

    messages: send messages in realtime and clear newMessage input

---
+++ app/channels/message_channel.rb
@@ -0,0 +1,10 @@
+class MessageChannel < ApplicationCable::Channel
+  def subscribed
+    stop_all_streams
+    stream_for Channel.find(params['id'])
+  end
+
+  def unsubscribed
+    stop_all_streams
+  end
+end
