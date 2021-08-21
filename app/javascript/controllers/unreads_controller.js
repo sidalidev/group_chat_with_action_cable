17fc337259da8aa110f0c4c49d024bf7e6bdc282
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-08-21 07:17:54 +0200

    channels: unread messages

---
+++ app/javascript/controllers/unreads_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "stimulus"
+import consumer from 'channels/consumer'
+
+export default class extends Controller {
+  connect() {
+    this.subscription = consumer.subscriptions.create({ channel: 'UnreadsChannel', id: this.data.get('id') }, {
+      connected: this._connected.bind(this),
+      disconnected: this._disconnected.bind(this),
+      received: this._received.bind(this),
+    })
+  }
+
+  disconnect() {
+    consumer.subscriptions.remove(this.subscription)
+  }
+
+  _connected() {
+
+  }
+
+  _disconnected() {
+
+  }
+
+  _received(data) {
+    this.element.classList.add('font-extrabold')
+  }
+}
